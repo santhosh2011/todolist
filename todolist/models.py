839185fee5bfa26ff6d0f41ee0b08c0693b30290
Santhosh Thota <santhosh2011@gmail.com>
2023-09-01 16:43:36 +0530

    Altering created_at field

--- todolist/models.py
+++ todolist/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Todo(models.Model):
     description = models.TextField()
     completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
